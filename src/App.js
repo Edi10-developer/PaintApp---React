a866c063070943e358d3823c06f9ed3f810e578b
Edi <selimi.edi.1991@gmail.com>
2021-03-19 16:16:28 +0100

    Setted 3 components and style

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import randomColor from 'randomcolor'
+import Paint from './components/Paint'
 
 function App() {
+  const [count, setCount] = useState(0)
+
+  const increment = () => setCount(prevCount => prevCount + 1)
+  const decrement = () => setCount(prevCount => prevCount - 1)
+
+  const [color, setColor] = useState(0)
+  useEffect(()=>{
+    setColor(randomColor())
+  }, [count])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Paint App</h1>
-      </header>
+    <div style={{ borderTop: `10px solid ${color}`}}>
+      <Paint />
     </div>
-  );
+  )
 }
 
 export default App;
