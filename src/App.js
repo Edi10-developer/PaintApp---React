e4670547f4872e77eb91190d7273618341a5ecdb
Edi <selimi.edi.1991@gmail.com>
2021-03-19 13:00:26 +0100

    First commit
    Just setted the project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Paint App</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
