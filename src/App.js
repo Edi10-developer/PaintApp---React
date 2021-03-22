baa43c7ea474cf1bcb04b6c0f1f4a504b28aa707
Edi <selimi.edi.1991@gmail.com>
2021-03-22 14:02:54 +0100

    New components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import randomColor from 'randomcolor'
+
 import Paint from './components/Paint'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  const increment = () => setCount(prevCount => prevCount + 1)
-  const decrement = () => setCount(prevCount => prevCount - 1)
-
-  const [color, setColor] = useState(0)
-  useEffect(()=>{
+export default function Playground() {
+  const [count, setCount] = useState(30)
+  
+  const inputRef = useRef()
+  
+  const [color, setColor] = useState(null)
+  useEffect(() => {
     setColor(randomColor())
+    //inputRef.current.focus()
   }, [count])
-
+  
   return (
     <div style={{ borderTop: `10px solid ${color}`}}>
-      <Paint />
+     <Paint />
     </div>
   )
 }
-
-export default App;
